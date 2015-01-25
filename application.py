602c36b5b749a50297e179ee56f0435fd40c72b5
Jan Milosh <janmilosh@gmail.com>
2015-01-25 10:02:46 -0500

    Updates application.py for refactor

--- application.py
+++ application.py
@@ -1,14 +1,16 @@
 import time
 
-import scrape.get_company_links as links
+import scrape.get_company_links as get_company_links
 import scrape.get_company_pages as pages
 import scrape.parse_pages as parse
 
 def main():
-    counties = ['Delaware', 'Morrow', 'Marion']
+    # counties = ['Delaware', 'Morrow', 'Marion']
+    counties = ['Lucas', 'Morrow']
 
     for county in counties:
-        links.get_links(county)
+        company_links = get_company_links.CompanyLinks(county)
+        company_links.get_links()
         pages.get_pages(county)
         parse.create_csv(county)
         time.sleep(10)
